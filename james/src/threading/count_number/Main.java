f85712a5beb31619496301cdd282a99bb38d5ced
Hinh Bai <haibinh.061296@gmail.com>
2022-09-05 09:58:32 +0700

    đếm số

---
+++ james/src/threading/count_number/Main.java
@@ -0,0 +1,16 @@
+package threading.count_number;
+
+public class Main {
+    public static void main(String[] args) {
+        Count count = new Count();
+        try {
+            while (count.getMyThread().isAlive()) {
+                System.out.println("Main thread will be alive till the child thread is live");
+                Thread.sleep(1500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Main thread interrupted");
+        }
+        System.out.println("Main thread run is over");
+    }
+}
